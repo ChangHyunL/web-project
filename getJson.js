ebf20a853454b451e18ef4ee01b385f4fef7c1f3
lee <dlckdgus200011@gmail.com>
2022-12-30 22:58:42 +0900

    1230 파일 합치기 전

--- getJson.js
+++ getJson.js
@@ -1,17 +1,21 @@
 async function getJson() {
-  const res = await fetch(data.json);
+  const res = await fetch('./data.json');
   const data = await res.json();
   let jsonData = "";
 
-  if (button.innerHTML == "좋아하는 음식") jsonData += data.food;
-  if (button.innerHTML == "좋아하는 거") jsonData += data.like;
-  if (button.innerHTML == "취미") jsonData += data.hobby;
-  if (button.innerHTML == "하고싶은 말") jsonData += data.wish;
-  if (button.innerHTML == "좋아하는 말") jsonData += data.word;
-  console.log(jsonData);
+  if (btn[0].innerHTML == "좋아하는 거") jsonData = data.like;
+  if (btn[1].innerHTML == "좋아하는 음식") jsonData = data.food;
+  if (btn[2].innerHTML == "취미") jsonData = data.hobby;
+  if (btn[3].innerHTML == "좋아하는 말") jsonData = data.word;
+  if (btn[4].innerHTML == "하고싶은 말") jsonData = data.wish;
+
   result.innerHTML = jsonData;
+  console.log(result.innerHTML);
 }
 
 const result = document.querySelector("#result");
-const button = document.querySelector("#button");
-button.addEventListener("click", getJson);
+const btn = document.getElementsByClassName("button");
+btn.addEventListener('click', getJson);
+for (let i =0; i<btn.length;i++) {
+  console.log(btn[i].innerHTML)
+}
