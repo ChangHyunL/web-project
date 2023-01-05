c319e40d40dc8fcde3daa93e84b56740031de470
lee <dlckdgus200011@gmail.com>
2023-01-05 14:52:04 +0900

    0105 commit

--- public/getJson.js
+++ public/getJson.js
@@ -1,16 +1,19 @@
 async function getJson() {
   const res = await fetch("./data.json");
   const data = await res.json();
-  const type = data.map(e=>e.type);
-  const content = data.map(e=>e.content);
-  const src = data.map(e=>e.src);
-  const slides = document.querySelector('.slides');
+  const type = data.map((e) => e.type);
+  const content = data.map((e) => e.content);
+  const src = data.map((e) => e.src);
+  const slides = document.querySelector(".slides");
 
-  for (let i = 0; i<type.length;i++) {
-    const dataType = document.createElement('p');
-    const dataImage = document.createElement('img');
-    const dataContent = document.createElement('p');
-    const list = document.createElement('li');
+  const slide___size = 330 * data.length - 30;
+  slides.style.width = slide___size + "px";
+
+  for (let i = 0; i < data.length; i++) {
+    const dataType = document.createElement("p");
+    const dataImage = document.createElement("img");
+    const dataContent = document.createElement("p");
+    const list = document.createElement("li");
     slides.appendChild(list);
     dataType.innerText = type[i];
     dataImage.src = src[i];
